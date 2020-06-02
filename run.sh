61548d0fede0428fe3771013ae9eeee135d197e9
lilong <lilong@qutoutiao.net>
2020-06-02 15:13:08 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-wget https://storage.googleapis.com/bert_models/2018_11_03/chinese_L-12_H-768_A-12.zip
-unzip chinese_L-12_H-768_A-12.zip
+
+path="chinese_L-12_H-768_A-12"
+if [ ! -d ${path} ]; then
+    wget https://storage.googleapis.com/bert_models/2018_11_03/chinese_L-12_H-768_A-12.zip
+    unzip chinese_L-12_H-768_A-12.zip
+fi
 python controller.py train
 python controller.py predict
